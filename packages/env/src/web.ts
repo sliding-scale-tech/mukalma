6426395ca8be61fd10a9198543215980301d896b
Areeburrehman-26 <areebrehman615@gmail.com>
2026-06-18 10:49:54 +0500

    initial commit

---
+++ packages/env/src/web.ts
@@ -0,0 +1,13 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const env = createEnv({
+  clientPrefix: "VITE_",
+  client: {
+    VITE_CONVEX_URL: z.url(),
+    VITE_CLERK_PUBLISHABLE_KEY: z.string().min(1),
+  },
+  runtimeEnv: (import.meta as any).env,
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  emptyStringAsUndefined: true,
+});
